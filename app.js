8b13dec3f4a069384876dedc2e42f4e9d2c3c649
Devansh Sahu <devanshsahumumbai@gmail.com>
2024-06-10 21:49:47 +0530

    basic error handling

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const ejsMate = require('ejs-mate')
+const path = require("path")
+const methodOverride = require("method-override")
+
+const Listing = require("./models/listing.js")
+const wrapAsync = require('./utils/wrapAsync.js')
+const ExpressError = require("./utils/ExpressError.js")
+const {listingSchema} = require('./schema.js')
+
+// Init Express
 const app = express()
 const PORT = 8080
 
-const mongoose = require("mongoose")
+// Init DB
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" 
-const Listing = require("./models/listing.js")
 
-const ejs = require("ejs")
-const ejsMate = require('ejs-mate')
-const path = require("path")
+// Init ejs-mate
 app.engine('ejs', ejsMate)
+
+// Init ejs render
 app.set("view engine", 'ejs')
 app.set("views", path.join(__dirname, "/views"))
 
+// For POST and PUT
 app.use(express.urlencoded({extended:true}))
-
-app.use(express.static(path.join(__dirname, "/public")))
-
-const methodOverride = require("method-override")
 app.use(methodOverride('_method'))
 
+// For static files
+app.use(express.static(path.join(__dirname, "/public")))
 
-console.log(ejsMate.layout)
-
+// Connect to DB
 async function main(){
     await mongoose.connect(MONGO_URL)
 }
@@ -33,62 +41,95 @@ main().then(()=>{
     console.log(err)
 })
 
+// Schema Validation
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body)
+    if(error) {
+        let errMsg = error.details.map((el)=>el.message).join(", ")
+        throw new ExpressError(400, errMsg)
+    } else {
+        next()
+    }
+}
+
+// Root
 app.get("/", (req, res)=>{
     res.send('This is root...')
 })
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My house",
-        description: ".wad",
-        price: 1500,
-        location: "unknown",
-        country: "doom"
-    })
-    await sampleListing.save();
-    console.log("Saved")
-    res.send("Test successful")
-})
-
-app.get("/listings", async (req, res) => {
+// All Listings
+app.get("/listings", validateListing, wrapAsync(async (req, res) => {
     const allListings = await Listing.find({})
     res.render("./listings/index.ejs", { allListings })
-})
+}))
 
+// Create New Listing
 app.get("/listings/new", (req, res) => {
     res.render("./listings/new.ejs")
 })
 
-app.post("/listings", async (req, res) => {
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     let newListing = new Listing(req.body)
     await newListing.save();
     res.redirect("/listings")
-})
+}))
 
-app.get("/listings/:id", async (req, res) => {
+// Show Listing
+app.get("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params
     const thisListing = await Listing.findById(id)
     res.render("./listings/show.ejs", {thisListing})
-})
+}))
 
-app.get("/listings/:id/edit", async (req, res) => {
+// Edit Listing
+app.get("/listings/:id/edit", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params
     const thisListing = await Listing.findById(id)
     res.render("./listings/edit.ejs", {thisListing})
-})
+}))
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    if(!req.body) {
+        throw new ExpressError(400, "Send valid data for listing")
+    }
     let {id} = req.params
     await Listing.findByIdAndUpdate(id, {...req.body})
     res.redirect("/listings")
-})
+}))
 
-app.delete("/listings/:id", async (req, res) => {
+// Delete Listing
+app.delete("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let {id} = req.params
     let deletedListing = await Listing.findByIdAndDelete(id)
     res.redirect("/listings")
+}))
+
+// 404 Error
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"))
 })
 
+// Error Handling
+app.use((err, req, res, next)=>{
+    let {statusCode=500, message="Something went wrong!"} = err
+    res.status(statusCode).render("error.ejs", {statusCode, message})
+})
+
+// Listening
 app.listen(PORT, ()=>{
     console.log(`Listening to port ${PORT}`)
 })
+
+// Test Listing
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My house",
+//         description: ".wad",
+//         price: 1500,
+//         location: "unknown",
+//         country: "doom"
+//     })
+//     await sampleListing.save();
+//     console.log("Saved")
+//     res.send("Test successful")
+// })
